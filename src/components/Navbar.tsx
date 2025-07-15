9dafad9a77094cab216da3635d32d5454ac1e59c
Harshhu045 <upadhyayh886@gmail.com>
2025-07-15 14:14:25 +0530

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,15 @@ const Navbar = () => {
   return (
     <div className="flex justify-around p-3 m-3 text-center items-center">
         <div><img src="https://programmingpathshala.com/_next/static/media/PpaLogo.d30bfdd2.svg" alt="" /></div>
-        <div className="flex justify-center gap-4 items-center">
-            <p className="text-lg text-gray-700">Explore courses</p>
-            <p>Success stories</p>
-            <p>Blogs</p>
-            <p>Events</p>
+        <div className="flex justify-center gap-6 items-center">
+            <p className="text-lg text-gray-700 cursor-pointer">Explore courses<span></span></p>
+            <p className="text-lg text-gray-700 cursor-pointer">Success stories</p>
+            <p className="text-lg text-gray-700 cursor-pointer">Blogs</p>
+            <p className="text-lg text-gray-700 cursor-pointer">Events</p>
         </div>
-        <div className="flex justify-center items-center">
-            <button>Log in</button>
-            <button>Start free Trial</button>
+        <div className="flex justify-center items-center gap-4">
+            <button className='w-20 h-8 bg-buttobg rounded hover:scale-105 cursor-pointer'>Log in</button>
+            <button className='w-28 h-8 bg-buttobg rounded hover:scale-105 cursor-pointer'>Start free Trial</button>
         </div>
     </div>
   )
